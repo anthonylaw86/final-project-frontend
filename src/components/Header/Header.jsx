8e4728b752b2bf74fd02878869a6cf5fabc87c82
AnthonyLaw <anthonylaw910@gmail.com>
2024-06-25 11:55:58 -0600

    responsive design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,32 +8,34 @@ function Header({ handleLoginModal, isLoggedIn }) {
   const currentUser = useContext(CurrentUserContext);
 
   return (
-    <header className="header">
-      <div>
-        <Link to="/" className="header__link">
-          <img src={MusicEmoji} alt="music emoji" className="header__emoji" />
-          <p className="header__logo">gimme a beat</p>
-        </Link>
-      </div>
-      <div className="header__button-container">
-        <Link to="/" className="header__link">
-          <p className="header__home-link">Home</p>
-        </Link>
-        {isLoggedIn ? (
-          <Link to="/profile" className="header__link">
-            <p className="header__username">{currentUser?.name}</p>
+    <div className="header__container">
+      <header className="header">
+        <div>
+          <Link to="/" className="header__link">
+            <img src={MusicEmoji} alt="music emoji" className="header__emoji" />
+            <p className="header__logo">gimme a beat</p>
           </Link>
-        ) : (
-          <button
-            onClick={handleLoginModal}
-            type="button"
-            className="header-button"
-          >
-            Sign In
-          </button>
-        )}
-      </div>
-    </header>
+        </div>
+        <div className="header__button-container">
+          <Link to="/" className="header__link">
+            <p className="header__home-link">Home</p>
+          </Link>
+          {isLoggedIn ? (
+            <Link to="/profile" className="header__link">
+              <p className="header__username">{currentUser?.name}</p>
+            </Link>
+          ) : (
+            <button
+              onClick={handleLoginModal}
+              type="button"
+              className="header-button"
+            >
+              Sign In
+            </button>
+          )}
+        </div>
+      </header>
+    </div>
   );
 }
 
