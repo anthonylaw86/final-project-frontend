b09cb6198cb31ff029915582b73d9320a1a2e412
AnthonyLaw <anthonylaw910@gmail.com>
2024-07-01 11:07:46 -0600

    kill me

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./components/App/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/se_project_react/">
+    <HashRouter basename="/final-project-frontend">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
