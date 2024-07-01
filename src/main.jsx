2d6081b67054c95453e6cf4f8e1eeddcd96acd93
AnthonyLaw <anthonylaw910@gmail.com>
2024-07-01 09:42:49 -0600

    adjustments

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/se_project_react">
+    <HashRouter basename="/final-project-frontend">
       <App />
     </HashRouter>
   </React.StrictMode>
