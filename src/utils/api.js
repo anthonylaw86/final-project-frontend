ab113ed468645c22bb2a2deb91826e9117e28d03
AnthonyLaw <anthonylaw910@gmail.com>
2024-08-08 11:06:53 -0600

    auth adjustments

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,80 +1,80 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://api.beatapp.strangled.net"
-    : "http://localhost:3001";
+    : "http://localhost:3002";
 
 export const loginUrl =
   process.env.NODE_ENV === "production"
     ? "https://beatapp.strangled.net/auth/login"
-    : "https://localhost:3001/auth/login";
+    : "https://localhost:3002/auth/login";
 
 export const checkResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Error ${res.status}`);
 };
 
-const getMusicItems = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const items = JSON.parse(localStorage.getItem("musicItems")) || [];
-      resolve(items);
-    }, 500);
-  });
-};
+// const getMusicItems = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       const items = JSON.parse(localStorage.getItem("musicItems")) || [];
+//       resolve(items);
+//     }, 500);
+//   });
+// };
 
-const addNewMusicItems = ({ name, artist, albumUrl }) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const items = JSON.parse(localStorage.getItem("musicItems")) || [];
-      const newItem = { id: Date.now(), name, artist, albumUrl, likes: 0 };
-      items.push(newItem);
-      localStorage.setItem("musicItems", JSON.stringify(items));
-      resolve(newItem);
-    }, 500);
-  });
-};
+// const addNewMusicItems = ({ name, artist, albumUrl }) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const items = JSON.parse(localStorage.getItem("musicItems")) || [];
+//       const newItem = { id: Date.now(), name, artist, albumUrl, likes: 0 };
+//       items.push(newItem);
+//       localStorage.setItem("musicItems", JSON.stringify(items));
+//       resolve(newItem);
+//     }, 500);
+//   });
+// };
 
-const deleteMusicItem = (id) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let items = JSON.parse(localStorage.getItem("musicItems")) || [];
-      items = items.filter((item) => item.id !== id);
-      localStorage.setItem("musicItems", JSON.stringify(items));
-      resolve({ message: "Item deleted successfully" });
-    }, 500);
-  });
-};
+// const deleteMusicItem = (id) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let items = JSON.parse(localStorage.getItem("musicItems")) || [];
+//       items = items.filter((item) => item.id !== id);
+//       localStorage.setItem("musicItems", JSON.stringify(items));
+//       resolve({ message: "Item deleted successfully" });
+//     }, 500);
+//   });
+// };
 
-const addLike = (id) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const items = JSON.parse(localStorage.getItem("musicItems")) || [];
-      const item = items.find((item) => item.id === id);
-      if (item) {
-        item.likes += 1;
-        localStorage.setItem("musicItems", JSON.stringify(items));
-        resolve(item);
-      } else {
-        reject({ message: "Item not found" });
-      }
-    }, 500);
-  });
-};
+// const addLike = (id) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const items = JSON.parse(localStorage.getItem("musicItems")) || [];
+//       const item = items.find((item) => item.id === id);
+//       if (item) {
+//         item.likes += 1;
+//         localStorage.setItem("musicItems", JSON.stringify(items));
+//         resolve(item);
+//       } else {
+//         reject({ message: "Item not found" });
+//       }
+//     }, 500);
+//   });
+// };
 
-const removeLike = (id) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const items = JSON.parse(localStorage.getItem("musicItems")) || [];
-      const item = items.find((item) => item.id === id);
-      if (item) {
-        item.likes = Math.max(0, item.likes - 1); // Ensure likes don't go below 0
-        localStorage.setItem("musicItems", JSON.stringify(items));
-        resolve(item);
-      } else {
-        reject({ message: "Item not found" });
-      }
-    }, 500);
-  });
-};
+// const removeLike = (id) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const items = JSON.parse(localStorage.getItem("musicItems")) || [];
+//       const item = items.find((item) => item.id === id);
+//       if (item) {
+//         item.likes = Math.max(0, item.likes - 1); // Ensure likes don't go below 0
+//         localStorage.setItem("musicItems", JSON.stringify(items));
+//         resolve(item);
+//       } else {
+//         reject({ message: "Item not found" });
+//       }
+//     }, 500);
+//   });
+// };
 
 const fetchToken = async () => {
   const response = await fetch(`${baseUrl}/auth/token`);
@@ -88,53 +88,53 @@ const fetchToken = async () => {
 };
 
 // FOR BACKEND
-// const getMusicItems = () => {
-//   return fetch(`${baseUrl}/items`, {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   }).then(checkResponse);
-// };
+const getMusicItems = () => {
+  return fetch(`${baseUrl}/items`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(checkResponse);
+};
 
-// const addNewMusicItems = ({ name, artist, albumUrl }, token) => {
-//   return fetch(`${baseUrl}/items`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       authorization: `Bearer ${token}`,
-//     },
-//     body: JSON.stringify({ name, artist, albumUrl }),
-//   }).then(checkResponse);
-// };
+const addNewMusicItems = ({ name, artist, albumUrl }, token) => {
+  return fetch(`${baseUrl}/items`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ name, artist, albumUrl }),
+  }).then(checkResponse);
+};
 
-// const deleteMusicItem = (id, token) => {
-//   return fetch(`${baseUrl}/items/${id}`, {
-//     method: "DELETE",
-//     headers: {
-//       "Content-Type": "application/json",
-//       authorization: `Bearer ${token}`,
-//     },
-//   }).then(checkResponse);
-// };
+const deleteMusicItem = (id, token) => {
+  return fetch(`${baseUrl}/items/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+};
 
-// const addLike = (id, token) => {
-//   return fetch(`${baseUrl}/items/${id}/likes`, {
-//     method: "PUT",
-//     headers: {
-//       authorization: `Bearer ${token}`,
-//     },
-//   }).then(checkResponse);
-// };
+const addLike = (id, token) => {
+  return fetch(`${baseUrl}/items/${id}/likes`, {
+    method: "PUT",
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+};
 
-// const removeLike = (id, token) => {
-//   return fetch(`${baseUrl}/items/${id}/likes`, {
-//     method: "DELETE",
-//     headers: {
-//       authorization: `Bearer ${token}`,
-//     },
-//   }).then(checkResponse);
-// };
+const removeLike = (id, token) => {
+  return fetch(`${baseUrl}/items/${id}/likes`, {
+    method: "DELETE",
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+};
 
 const api = {
   removeLike,
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,106 +1,45 @@
 import { baseUrl } from "./api";
 import { checkResponse } from "./api";
 
-// export const signUp = ({ email, password, username }) => {
-//   return new Promise((resolve, reject) => {
-//     // Simulate a delay for the server response
-//     setTimeout(() => {
-//       const users = JSON.parse(localStorage.getItem("users")) || [];
-
-//       // Check if the user already exists
-//       if (users.some((user) => user.email === email)) {
-//         reject({ message: "User already exists" });
-//       } else {
-//         const newUser = { email, password, username };
-//         users.push(newUser);
-//         localStorage.setItem("users", JSON.stringify(users));
-//         resolve({ message: "User registered successfully" });
-//       }
-//     }, 500);
-//   });
-// };
-
 export const signUp = ({ email, password, username }) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const users = JSON.parse(localStorage.getItem("users")) || [];
-      console.log("Existing users: ", users); // Log existing users for debugging
-
-      if (users.some((user) => user.email === email)) {
-        reject({ message: "User already exists" });
-      } else {
-        const newUser = { email, password, username };
-        users.push(newUser);
-        localStorage.setItem("users", JSON.stringify(users));
-        resolve({ message: "User registered successfully" });
-      }
-    }, 500);
-  });
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, username }),
+  }).then(checkResponse);
 };
 
-export const signIn = ({ username, password }) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const users = JSON.parse(localStorage.getItem("users")) || [];
-      const user = users.find(
-        (user) => user.username === username && user.password === password
-      );
-
-      if (user) {
-        resolve({
-          message: "Sign in successful",
-          token: "fake-jwt-token",
-          user,
-        });
-      } else {
-        reject(new Error("Invalid email or password"));
-      }
-    }, 500);
-  });
+export const signIn = ({ email, password }) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
 };
 
 export const checkToken = (token) => {
-  return new Promise((resolve, reject) => {
-    // Simulate token validation
-    setTimeout(() => {
-      if (token === "fake-jwt-token") {
-        resolve({ message: "Token is valid" });
-      } else {
-        reject({ message: "Invalid token" });
-      }
-    }, 500);
-  });
+  return fetch(`${baseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
 };
 
-// export const signUp = ({ email, password, username }) => {
-//   return fetch(`${baseUrl}/signup`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ email, password, username }),
-//   }).then(checkResponse);
-// };
-
-// export const signIn = ({ email, password }) => {
-//   return fetch(`${baseUrl}/signin`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ email, password }),
-//   }).then(checkResponse);
-// };
-
-// export const checkToken = (token) => {
-//   return fetch(`${baseUrl}/users/me`, {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//       authorization: `Bearer ${token}`,
-//     },
-//   }).then(checkResponse);
-// };
+export const getCurrentUser = (token) => {
+  return fetch(`${baseUrl}/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
 
 //-----------Mock Server-------//
 // export const checkToken = (token) => {
